2b5d49b6596f1a3977a879efc3ebc467f13beec1
Alex Kuzin <acidkarma3000@gmail.com>
2023-05-10 13:43:00 +0500

    Rename2

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -2,15 +2,12 @@ import java.math.BigInteger;
 
 public class CreditPaymentService {
     public double calculate(int b, int a) {
-        float r = (float)15 / 100 / 12; // Ставка в месяц
-        double m = b * ((r * Math.pow((1 + r), 12 * a)) / (Math.pow((1 + r), 12 * a) - 1));
-        double s = 12 * a * m;
-        System.out.println("Платеж: " + m);
-        System.out.println("Сумма: " + s);
-
-
-         return m;
-
-
+        float interestRate = (float) 9.99 / 100 / 12; // Ставка в месяц
+        float calculation = (float) Math.pow(1 + interestRate, a);
+        float calculation2 = calculation * interestRate;
+        float calculation3 = calculation - 1;
+        float calculation4 = calculation2 / calculation3;
+        float annuityPayment = Math.round(b * calculation4);
+        return annuityPayment;
         }
     }
